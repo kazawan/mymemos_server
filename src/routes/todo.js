0082d27628bcb7eb3c938804f5841909ec64837f
kazawan <55086403@qq.com>
2023-04-24 15:23:17 +0800

    modified:   mymemos 	modified:   src/db/db.js 	modified:   src/db/todolist.js 	modified:   src/jwt/jwtinit.js 	modified:   src/routes/router.js 	modified:   src/use/useapp.js

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,16 +1,59 @@
-const {todolist_dbAll} =require ('../db/todolist')
-
+const { todolist_dbAll } = require('../db/todolist')
+const { todolist_dbUser } = require('../db/todolist')
+const {todolistAdd_dbUpdate} = require('../db/todolist')
 /**
  * * 获取所有todo
  * todo 添加增加todo的功能
  */
-const todo_all = async (req,res) =>{
+const todo_all = async (req, res) => {
     let temp = await todolist_dbAll()
+    console.log(temp)
     res.send({
-        code:200,
-        msg:'success',
-        data:JSON.parse(temp[0].json)
+        code: 200,
+        msg: 'success',
+        data: temp[1].username
     })
 }
 
-module.exports = {todo_all}
+/**
+ * * 获取指定用户的todo
+ */
+
+const todo_user = async (req, res) => {
+    let temp = await todolist_dbUser(req.body.username)
+    // console.log(temp)
+    if(temp === undefined){
+        res.send({
+            code: 400,
+            msg: 'fail',
+            data: 'no user'
+        })
+        return
+    }else{
+        res.send({
+            code: 200,
+            msg: 'success',
+            data:temp
+        })
+    }
+    
+}
+
+/**
+ * * 添加todo
+ */
+const addtodo = async (req, res) => {
+    console.log()
+    let todo = JSON.stringify(req.body.json)
+    let temp = await todolistAdd_dbUpdate(req.body.username,todo)
+    res.send(temp)
+    // console.log(todo)
+    //  todolistAdd_dbUpdate(req.body.username,todo)
+    //     .then(res=>console.log(res))
+
+}
+module.exports = {
+    todo_all,
+    todo_user,
+    addtodo,
+}
